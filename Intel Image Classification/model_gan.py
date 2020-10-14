b92f9e6ee30c769d1f66f0763b95e768a7fbb967
Reo Sahobby <reoshby@gmail.com>
2020-10-14 09:30:07 +0700

    Create model_gan.py

---
+++ Intel Image Classification/model_gan.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 09:22:57 2020
+
+@author: ROG
+"""
+
+
+import torch
+from torch import nn
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(22500, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 64),
+            nn.LeakyReLU(),
+            nn.Linear(64, 1),
+            nn.Sigmoid()
+            )
+    
+    def forward(self, x):
+        return self.fc(x)
+    
+    
+class Generator(nn.Module):
+    
