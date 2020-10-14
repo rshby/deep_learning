cac9a4b230a0facd9a65104f1baf5d20946d7964
Reo Sahobby <reoshby@gmail.com>
2020-10-14 09:38:38 +0700

    Update model_gan.py

--- Intel Image Classification/model_gan.py
+++ Intel Image Classification/model_gan.py
@@ -2,9 +2,8 @@
 """
 Created on Wed Oct 14 09:22:57 2020
 
-@author: ROG
+@author: Reo Sahobby
 """
-
 
 import torch
 from torch import nn
@@ -37,5 +36,30 @@ class Generator(nn.Module):
         super().__init__()
         self.z_dimenssion = z_dimenssion
         self.fc = nn.Sequential(
-            nn.Linear(z_dimenssion, out_features)
+            nn.Linear(z_dimenssion, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 4096),
+            nn.BatchNorm1d(4096),
+            nn.LeakyReLU(),
+            nn.Linear(4096, 16384),
+            nn.BatchNorm1d(16384),
+            nn.LeakyReLU(),
+            nn.Linear(16384, 22500),
+            nn.Tanh()
             )
+        
+    def forward(self, x):
+        return self.fc(x)
+    
+    def generate(self, n, device):
+        fake_image = torch.rand((n, self.z_dimenssion), device=device)
+        return self.fc(fake_image)
